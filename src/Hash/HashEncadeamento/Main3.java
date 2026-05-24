11d42b860359422b6fb03d8ae903c619f10afcb4
GarrardFontanive <car.fontanive@gmail.com>
2026-05-24 14:41:06 -0300

    Corrigido hashEncadeamento que estava com Main incompleto

--- src/Hash/HashEncadeamento/Main3.java
+++ src/Hash/HashEncadeamento/Main3.java
@@ -1,29 +1,28 @@
 package Hash.HashEncadeamento;
-
 public class Main3 {
+
     public static void main(String[] args) {
-        HashTable3 agenda = new HashTable3(4);
 
-        agenda.inserir("cauane", "(47) 9999-9999");
-        agenda.inserir("victor", "(47) 8888-8888");
+        HashTable3 agenda = new HashTable3(10000);
 
-        System.out.println("--- Tabela Inicial ---");
-        agenda.mostrarTabela();
-
-        // "victor" e "torvic" têm as mesmas letras, gerando o mesmo hash.
-        // Como sua tabela usa encadeamento, eles vão ficar no mesmo índice!
-        agenda.inserir("torvic", "(47) 7777-7777");
-
-        System.out.println("\n--- Após Colisão (victor e torvic) ---");
-        agenda.mostrarTabela();
-
-        agenda.inserir("pedro", "(47) 1111-1111");
-
-        System.out.println("\n--- Tabela Final ---");
-        agenda.mostrarTabela();
-
-        agenda.deletarChave("torvic");
-        System.out.println("\n--- Tabela Final ---");
-        agenda.mostrarTabela();
+//		agenda.inserir("Carlos", "999");
+//		agenda.inserir("Kelim", "777");
+//		agenda.inserir("solCar", "222");
+//
+//		agenda.mostrarTabela();
+//		System.out.println(agenda.recuperar("Carlos"));
+//
+//		agenda.inserir("Roberto Carlos", "99932");
+//		agenda.mostrarTabela();
+//
+//		agenda.removerChave("solCar");
+//		agenda.mostrarTabela();
+        System.out.println(System.currentTimeMillis());
+        for(int i =0; i<10000;i++) {
+            agenda.inserir("chave"+i, "valor"+i);
+        }
+        System.out.println(System.currentTimeMillis());
+//	agenda.mostrarTabela();
     }
+
 }
