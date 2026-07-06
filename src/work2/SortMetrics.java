b35d59cd35e8b57f4aa23268a35496032b05d3a3
GarrardFontanive <car.fontanive@gmail.com>
2026-07-06 10:36:33 -0300

    Correção da classe de métricas de ordenação

--- src/work2/SortMetrics.java
+++ src/work2/SortMetrics.java
@@ -1,39 +1,40 @@
 package work2;
 
 public class SortMetrics {
+
     private long comparacoes;
     private long movimentacoes;
-    private long tempoExecucaoMs;
+    private long tempoExecucaoNs;
 
-    void incrementarComparacoes() {
+    public void incrementarComparacoes() {
         comparacoes++;
     }
 
-    void incrementarMovimentacoes() {
+    public void incrementarMovimentacoes() {
         movimentacoes++;
     }
 
-    void adicionarMovimentacoes(long quantidade) {
+    public void adicionarMovimentacoes(long quantidade) {
         movimentacoes += quantidade;
     }
 
-    long getComparacoes() {
+    public long getComparacoes() {
         return comparacoes;
     }
 
-    long getMovimentacoes() {
-        return comparacoes;
+    public long getMovimentacoes() {
+        return movimentacoes;
     }
 
-    long getTempoExecucaoNs() {
-        return tempoExecucaoMs;
+    public long getTempoExecucaoNs() {
+        return tempoExecucaoNs;
     }
 
-    double getTempoExecicaoMs() {
-        return tempoExecucaoMs / 1_000_000.0;
+    public double getTempoExecucaoMs() {
+        return tempoExecucaoNs / 1_000_000.0;
     }
 
-    void setTempoExecucaoNs(long tempoExecucaoNs) {
-        this.tempoExecucaoMs = tempoExecucaoNs;
+    public void setTempoExecucaoNs(long tempoExecucaoNs) {
+        this.tempoExecucaoNs = tempoExecucaoNs;
     }
 }
