27b253c46a4d916611aff957ad0129728a10bf07
GarrardFontanive <car.fontanive@gmail.com>
2026-07-05 22:53:01 -0300

    Criando classe de métricas de ordenação

--- src/work2/SortMetrics.java
+++ src/work2/SortMetrics.java
@@ -1,4 +1,39 @@
 package work2;
 
 public class SortMetrics {
+    private long comparacoes;
+    private long movimentacoes;
+    private long tempoExecucaoNs;
+
+    void incrementarComparacoes() {
+        comparacoes++;
+    }
+
+    void incrementarMovimentacoes() {
+        movimentacoes++;
+    }
+
+    void adicionarMovimentacoes(long quantidade) {
+        movimentacoes += quantidade;
+    }
+
+    long getComparacoes() {
+        return comparacoes;
+    }
+
+    long getMovimentacoes() {
+        return comparacoes;
+    }
+
+    long getTempoExecucaoNs() {
+        return tempoExecucaoNs;
+    }
+
+    double getTempoExecicaoMs() {
+        return tempoExecucaoNs / 1_000_000.0;
+    }
+
+    void setTempoExecucaoNs(long tempoExecucaoNs) {
+        this.tempoExecucaoNs = tempoExecucaoNs;
+    }
 }
