1f12f8525ce02527f55ced82040ec45c6ad9c5bc
GarrardFontanive <car.fontanive@gmail.com>
2026-07-05 23:13:21 -0300

    1⁰ - Bubble sort com implementação do Sorter

--- src/work2/BubbleSort.java
+++ src/work2/BubbleSort.java
@@ -1,4 +1,33 @@
 package work2;
 
-public class BubbleSort {
+public class BubbleSort implements Sorter{
+    @Override
+    public String getNome() {
+        return "Bubble Sort";
+    }
+
+    @Override
+    public void sort(int[] vetor, SortMetrics metricas) {
+        int n = vetor.length;
+        boolean trocou;
+
+        for (int i = 0 ; i < n - 1 ; i++) {
+            trocou = false;
+
+            for (int j = 0; j < n - 1 - i ; j++) {
+                metricas.incrementarComparacoes();
+
+                if (vetor[j] > vetor[j - 1]) {
+                    int temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor [j + 1] = temp;
+
+                    metricas.adicionarMovimentacoes(3);
+                    trocou = true;
+                }
+            }
+
+            if(!trocou) break;
+        }
+    }
 }
